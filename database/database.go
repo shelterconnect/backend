5ae87d90ccd449bcff810e8b5f661c870f3f0d13
Zach Latta <zach@zachlatta.com>
2014-05-31 13:44:49 -0600

    Set up database.

---
+++ database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq" // Postgres driver
+)
+
+var db *sql.DB
+
+// Init initializes the internal database handle.
+func Init(name, datasource string) error {
+	var err error
+	db, err = sql.Open(name, datasource)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Close database connection
+func Close() {
+	db.Close()
+}
